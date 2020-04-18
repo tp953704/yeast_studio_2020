d3965b302753b909fd4455a927a79d7533c19576
Wang <tp953704@gmail.com>
2020-04-18 23:55:15 +0800

    亞軍生氣

--- js/index_05.js
+++ js/index_05.js
@@ -4,11 +4,11 @@ $(function(){
             if($('.js-triggle').hasClass('d-none')){
                 $('.area').css('height','120rem');
                 $('.js-triggle').removeClass('d-none');
-                $('.content_btn').find('.md-txt').html(closetxt);
+                $('.content_btn').find('.md-txt').html('隱藏');
             }else{
                 $('.js-triggle').addClass('d-none');
                 $('.area').css('height','0rem');
-                $('.content_btn').find('.md-txt').html(opentxt);
+                $('.content_btn').find('.md-txt').html('查看更多');
                 
             }
         }
